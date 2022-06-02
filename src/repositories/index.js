59e738826b17fe4340fe5c650e481a8445c61ae4
Felipe Braga <felipebraga95@hotmail.com>
2022-06-01 23:07:31 -0300

    CRUD senhas

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,7 +1,10 @@
 const FilaRepository = require('./filaRepository.js')
 const ClienteRepository = require('./clienteRepository.js')
+const SenhaRepository = require('./senhaRepository.js')
 
 module.exports = {
   FilaRepository,
-  ClienteRepository
+  ClienteRepository,
+  SenhaRepository
+
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const bodyParser = require('body-parser')
 const fila = require('./fila-route')
 const cliente = require('./cliente-route')
+const senha = require('./senha-route')
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
     fila,
-    cliente
+    cliente,
+    senha
 
   )
 }
