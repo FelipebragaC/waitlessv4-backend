4e5fac01d61c5e63c8baf7e17a2a0ccd6b52b878
Felipe Braga <felipebraga95@hotmail.com>
2022-06-27 23:48:27 -0300

    refactory auth

--- src/routes/login-route.js
+++ src/routes/login-route.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
+const { authUser } = require('../auth/userAuthentication.js')
 const LoginController = require('../controllers/loginController')
 const router = Router()
 
 router
-  .get('/login', LoginController.authUser, LoginController.getUsers)
+  .get('/login', authUser, LoginController.getUsers)
   .get('/login/:id', LoginController.getSingleUser)
   .get('/login/nome/:emailAddress', LoginController.getUserName)
   .post('/login', LoginController.insertUser)
