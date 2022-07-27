c538e20e1755a42764d889c280b9f30ae0840a18
Felipe Braga <felipebraga95@hotmail.com>
2022-07-27 12:53:04 -0300

    Alteração test

--- src/test/clienteController.spec.js
+++ src/test/clienteController.spec.js
@@ -1,5 +1,7 @@
-
-
+const mockRequest = (sessionData, bodyData) => ({
+  session: { data: sessionData },
+  body: bodyData
+});
 
 const mockResponse = () => {
   const res = {};
@@ -7,14 +9,16 @@ const mockResponse = () => {
   res.json = jest.fn().mockReturnValue(res);
   return res;
 };
+
 const ClienteController = require('../controllers/clienteController')
 describe('Client Controller', () => {
 
-  test('should return 200', async  () => {
+
+  test('should return 200', async () => {
     const req = mockRequest({});
     const res = mockResponse();
-    await ClienteController.getClients(req,res)
+    await ClienteController.getClients(req, res)
 
-    expect(res.status).toBe(200)
+    expect(res.status).toHaveBeenCalledWith(200)
   })
 })
